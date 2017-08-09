80cc946b2f17eed4c80db35b03f64f0e20d5d522
alejandro <alejandrozapata73@gmail.com>
2017-08-08 22:40:56 -0300

    Cuestionario, pequeñas estructuras

---
+++ src/Repaso.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose.zapatagom
+ */
+public class Repaso {
+    
+    public static void main(String[] args){
+    
+    
+    /*
+        Pregunta 1:
+        Cuál sería la salida del siguiente código:
+
+        int x = 9;
+        while(x >  0){
+     System.out.print(x);
+     x--;
+        }
+
+        Pregunta 2:
+        Cuál sería la salida del siguiente código:
+
+        for(int i =0; i<10;i++){
+         if(i == 7){
+        break;
+         }
+          System.out.println(i);
+        }
+
+        Pregunta 4:
+        Qué tipo de valor tiene que pasarse a la estructura if?
+    
+        Pregunta 5:
+        Cuál sería la salida del siguiente código:
+
+        for (int i = 0; i <= 10; i++){
+        if( i == 8 ){
+               break;
+        }
+         System.out.println(i + " ");
+        }
+
+        Pregunta 6:
+        Cuál sería la salida del siguiente código:
+
+        int i = 0;
+ 
+        while(true){
+       if(i == 5){
+      continue;
+     }
+     if(i == 20){
+          break;
+      }
+      i+=2
+      System.out.println(i + " ");
+        }
+
+    
+    */
+    }
+
+}
